8c44ed2b447ca412151d894e627d0e375431ce53
cloud <cloud@cloudpc.(none)>
2016-05-23 17:16:05 +0800

    add file

--- shipblog/shipblog/settings.py
+++ shipblog/shipblog/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'shipblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'HOST': 'mysql',
+        'PORT':3306,
+        'USER':'root',
+        'PASSWORD': '111111'
     }
 }
 
