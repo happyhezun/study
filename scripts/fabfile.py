d60d77d5eac6f28fa8e3c2ffca69d078f08147e5
root <root@10.0.2.15>
2017-06-04 10:25:29 +0000

    delete file

--- scripts/fabfile.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-def lsfab():
-    local('cd /tmp')
-    local('ls')
--- scripts/in.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CNAME=$1
-CPID=$(docker inspect --format "{{.State.Pid}}" $CNAME)
-nsenter --target "$CPID" --mount --uts --ipc --net --pid
-
